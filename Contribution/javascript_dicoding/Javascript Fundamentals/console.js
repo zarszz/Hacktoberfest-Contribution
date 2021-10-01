96b83c97a4f609048a47df3782899a22f0661deb
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2021-10-01 20:05:32 +0700

    Add Frist Project

---
+++ Contribution/javascript_dicoding/Javascript Fundamentals/console.js
@@ -0,0 +1,7 @@
+
+    console.log(“Hello, World!”);
+
+
+    console.log("Nama Anda");
+
+// Tampilkan nama Anda ke layar pada kode di bawah:
---
+++ Contribution/javascript_dicoding/data structure/object.js
@@ -0,0 +1,19 @@
+
+// contoh
+let object = {key1: "Value1", key2: "Value2", key3: "Value3"};
+
+// contoh 2
+const user = {
+
+              firstName: "Faiz",
+
+              lastName: "Nurullah",
+
+              age: 17,
+
+              gender: "male"
+
+            };
+
+console.log(`Halo, Nama Saya ${user.firstName} ${user.lastName}`);
+console.log(`Umur Saya ${user.age} Tahun`);
